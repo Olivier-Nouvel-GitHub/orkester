3c3234c3306b8971f6829752dda98fdaf3e2e80a
Olivier <olivier.nvl@gmail.com>
2024-03-15 00:43:51 +0100

    chore: gitignore file updated to ignore .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e15d9335ce81a127"; }
+/******/ 		__webpack_require__.h = function() { return "0dfcdef23f828dfe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
